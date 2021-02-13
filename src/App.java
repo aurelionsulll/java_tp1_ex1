91386013b08ae42f0157627d9c2488a496bb3590
mehdi <mehdimn111@gmail.com>
2021-02-13 19:13:53 +0100

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        Produit p = new Produit();
+
+        System.out.println("entrez la categorie");
+        String categorie = sc.nextLine();
+        p.setCategorier(categorie);
+
+        System.out.println("entrez le prix hors taxe");
+        int prix_hors_taxe = sc.nextInt();
+        p.setPrix_hors_taxe(prix_hors_taxe);
+
+        System.out.println("quel est le nombre de produits dans le lot ? ");
+        int nProduit = sc.nextInt();
+        p.setNProduit(nProduit);
+        p.affiche();
+
     }
 }
---
+++ src/Produit.java
@@ -0,0 +1,54 @@
+public class Produit {
+
+    protected int prix_hors_taxe;
+    protected String categorie;
+    protected int nProduit;
+    protected double prix_ttc;
+
+    public Produit() {
+    }
+
+    public Produit(int prix_hors_taxe, String categorie) {
+        this.prix_hors_taxe = prix_hors_taxe;
+        this.categorie = categorie;
+    }
+
+    public int getPrix_hors_taxe() {
+        return this.prix_hors_taxe;
+    }
+
+    public void setPrix_hors_taxe(int prix_hors_taxe) {
+        this.prix_hors_taxe = prix_hors_taxe;
+    }
+
+    public String getCategorier() {
+        return this.categorie;
+    }
+
+    public void setCategorier(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public int getNProduit() {
+        return this.nProduit;
+    }
+
+    public void setNProduit(int nProduit) {
+        this.nProduit = nProduit;
+    }
+
+    public double ttc() {
+        if (categorie == "alimentaire") {
+            prix_ttc = prix_hors_taxe * 1.055;
+        } else {
+            prix_ttc = prix_hors_taxe * 1.20;
+        }
+        return prix_ttc;
+    }
+
+    public void affiche() {
+        ttc();
+        System.out.println("prix TTC d'un seul produit : " + prix_ttc + " prix TTC du lot :  : " + (prix_ttc * nProduit));
+    }
+
+}
